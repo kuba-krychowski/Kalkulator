9c6395625b0e83b1babd92f2901e1caa1b677237
kuba-krychowski <kkrychowski@interia.pl>
2018-04-25 20:48:52 +0200

    "dzialanie","liczbapierwsza","liczbadruga", "rodzajdzialania" changed for english words.

--- Source/WindowsFormsApp1/Form1.cs
+++ Source/WindowsFormsApp1/Form1.cs
@@ -14,9 +14,9 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        string Liczbapierwsza, Liczbadruga;
+        string firstnumber, secondnumber;
 
-        char Rodzajdzialania = ' ';
+        char typeoperation = ' ';
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -25,115 +25,115 @@ namespace WindowsFormsApp1
 
         private void button0_Click(object sender, EventArgs e)
         {
-            dzialanie(0);
+            action(0);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            dzialanie(1);
+            action(1);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            dzialanie(2);
+            action(2);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            dzialanie(3);
+            action(3);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            dzialanie(4);
+            action(4);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            dzialanie(5);
+            action(5);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            dzialanie(6);
+            action(6);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            dzialanie(7);
+            action(7);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            dzialanie(8);
+            action(8);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            dzialanie(9);
+            action(9);
         }
 
         private void buttonDodawanie_Click(object sender, EventArgs e)
         {
-            Rodzajdzialania = '+';
+            typeoperation = '+';
             tbWynik.Text = " ";
 
         }
 
         private void buttonOdejmowanie_Click(object sender, EventArgs e)
         {
-            Rodzajdzialania = '-';
+            typeoperation = '-';
             tbWynik.Text = " ";
         }
 
         private void buttonMnozenie_Click(object sender, EventArgs e)
         {
-            Rodzajdzialania = '*';
+            typeoperation = '*';
             tbWynik.Text = " ";
         }
 
         private void buttonDzielenie_Click(object sender, EventArgs e)
         {
-            Rodzajdzialania = '/';
+            typeoperation = '/';
             tbWynik.Text = " ";
         }
 
         private void buttonWynik_Click(object sender, EventArgs e)
         {
-            switch (Rodzajdzialania)  { 
+            switch (typeoperation)  { 
                 case ('+'):
-                    tbWynik.Text = (int.Parse(Liczbapierwsza) + int.Parse(Liczbadruga)).ToString();
+                    tbWynik.Text = (int.Parse(firstnumber) + int.Parse(secondnumber)).ToString();
                     break;
                 case ('-'):
-                    tbWynik.Text = (int.Parse(Liczbapierwsza) - int.Parse(Liczbadruga)).ToString();
+                    tbWynik.Text = (int.Parse(firstnumber) - int.Parse(secondnumber)).ToString();
                     break;
                 case ('/'):
-                    tbWynik.Text = (int.Parse(Liczbapierwsza) / int.Parse(Liczbadruga)).ToString();
+                    tbWynik.Text = (int.Parse(firstnumber) / int.Parse(secondnumber)).ToString();
                     break;
                 case ('*'):
-                    tbWynik.Text = (int.Parse(Liczbapierwsza) * int.Parse(Liczbadruga)).ToString();
+                    tbWynik.Text = (int.Parse(firstnumber) * int.Parse(secondnumber)).ToString();
                     break;
 
              }
-            Liczbapierwsza = "";
-            Liczbadruga = "";
-            Rodzajdzialania = ' ';
+           firstnumber = "";
+            secondnumber = "";
+            typeoperation = ' ';
         }
         private void tbWynik_TextChanged(object sender, EventArgs e)
         {
 
         }
-        private void dzialanie(int liczba)
+        private void action(int liczba)
         {
-             if(Rodzajdzialania == ' ')
+             if(typeoperation == ' ')
             {
-                Liczbapierwsza += liczba;
-                tbWynik.Text = Liczbapierwsza;
+                firstnumber += liczba;
+                tbWynik.Text = firstnumber;
             }
             else
             {
-                Liczbadruga += liczba;
-                tbWynik.Text = Liczbadruga;
+                secondnumber += liczba;
+                tbWynik.Text = secondnumber;
             }
         }      
     }    
